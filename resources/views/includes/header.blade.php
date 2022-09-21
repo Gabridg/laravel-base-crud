5db69ab57d08e97fc27840edef22504d49581e3e
Gabridg <gabri99.del@gmail.com>
2022-09-21 15:16:51 +0200

    created HomeController

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+    <div id="notify">
+        <div class="container">    
+            <span>DC POWER VISA&copy</span>
+            <span>OTHER DC SITES</span>
+        </div>
+    </div>
+    <div class="container">
+        <div class="header-nav">
+            <figure>
+                <img src="{{ asset('images/dc-logo.png')}} " alt="">
+            </figure>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="{{route('home')}}" class="active">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,34 +10,8 @@
 
 </head>
 <body>
-<header>
-    <div id="notify">
-        <div class="container">    
-            <span>DC POWER VISA&copy</span>
-            <span>OTHER DC SITES</span>
-        </div>
-    </div>
-    <div class="container">
-        <div class="header-nav">
-            <figure>
-                <img src="{{ asset('images/dc-logo.png')}} " alt="">
-            </figure>
-                <ul>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="{{route('home')}}" class="active">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-                </ul>
-        </div>
-    </div>
-</header>
-<main>
+@include('includes.header')
+<main>   
     @yield('main-conten')
 </main>
 </body>
