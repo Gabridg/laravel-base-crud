18cea2ab75b323a39ba3da31a76ecbbf160a09db
Gabridg <gabri99.del@gmail.com>
2022-09-21 16:38:37 +0200

    added comic &comic Card on index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.main')
 
 @section('main-content')
+<main class="main-content">
     <div class="container">
-        @dump($comics)
+        <div class="d-flex">
+
+            @forelse($comics as $comic)
+            <div class="comic-card">
+                <img src="{{$comic->thumb}}" alt="" class="comic">
+                <p class="comic-text">{{$comic->series}}</p>
+                <p class="comic-text">{{$comic->title}}</p>
+                <p class="comic-text">{{$comic->type}}</p>
+                <p class="comic-text">{{$comic->description}}</p>
+                <p class="comic-text">{{$comic->price}}</p>
+                <p class="comic-text">{{$comic->sale_date}}</p>
+            </div>
+            @empty
+            <h2>Attualmente non sono presenti fumetti</h2>
+            @endforelse
+        </div>
     </div>
+</main>
 @endsection
